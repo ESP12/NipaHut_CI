1b7799763c5f981bd5032fb2b8d327643e0fb4a5
Pamintuan Elijah <nipahutresort16@gmail.com>
2016-11-27 16:31:34 +0000

    Configuration Update: Base URL altered

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:7777/NipaHut_CI';
+$config['base_url'] = 'https://reservation-for-nipahut.herokuapp.com/NipaHut_Controller';
 
 /*
 |--------------------------------------------------------------------------
