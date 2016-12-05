214f43f300ca0b299234c2bab6159cef73bb7a86
Pamintuan Elijah <nipahutresort16@gmail.com>
2016-12-05 15:19:03 +0000

    Fix View REgistration

--- application/views/content/Registration/SignUp.php
+++ application/views/content/Registration/SignUp.php
@@ -1,44 +1,11 @@
-<?php
-if (isset($_POST['sbt-signup'])) {
-    ?>
-    <section class="reservation-content">
-        <div class="container">
-            <h1 class="text-center">Registration Successful <?php echo $_POST['register-firstname']; ?>!</h1><div class="row"><div class="col-md-3"></div><div class="col-md-6">
-                    <div class="panel panel-default"><div class="panel-heading summary-title">
-                            <h3 class="panel-title available-rooms-title">Guest Information</h3>
-                        </div>
-                        <div class=" list-group">
-                            <form method="POST" action="sign-up.php">
-                                <a class="list-group-item">
-                                    Username
-                                    <input name="register-username" type="text" class="form-control" readonly value="<?php echo $_POST['register-username']; ?>">
-                                </a>
-                                <a class="list-group-item">
-                                    First Name
-                                    <input name="register-firstname" type="text" class="form-control" readonly value="<?php echo $_POST['register-firstname']; ?>">
-                                </a>
-                                <a class="list-group-item">
-                                    Last Name
-                                    <input name="register-lastname" type="text" class="form-control" readonly value="<?php echo $_POST['register-lastname']; ?>">
-                                </a>
-                                <a class="list-group-item">
-                                    Gender
-                                    <input name="register-lastname" type="text" class="form-control" readonly value="<?php echo $_POST['register-gender']; ?>">
-                                </a>
-                                <a class="list-group-item">
-                                    Email Address
-                                    <input name="register-emailaddress" type="text" class="form-control" readonly value="<?php echo $_POST['register-emailaddress']; ?>">
-                                </a>
-                                <a class="list-group-item">
-                                    Mobile Number
-                                    <input name="register-mobilenumber" type="text" class="form-control" readonly value="<?php echo $_POST['register-mobilenumber']; ?>">
-                                </a>
-                            </form>
-                        </div></div>
-                </div>
-            </div></div></div>
-    </section>
-<?php } else { ?>
+<section class="transaction-image">
+    <div class="welcome-nipa">
+        <div class="centered-title no-border">
+            <h1 class="headline">Sign Up For A Nipa Hut Account Today!</h1>
+        </div>
+    </div>
+</section>
+
     <section class="reservation-content">
         <div class="container"><div class="row"><div class="col-md-3"></div><div class="col-md-6">
                     <div class="panel panel-default"><div class="panel-heading summary-title">
@@ -47,7 +14,7 @@ if (isset($_POST['sbt-signup'])) {
                         <div class=" list-group">
 
                             <?php echo validation_errors();?>
-                            <?php echo form_open('ReservationPro/SignUp');?>
+                            <?php echo form_open('content/Registration/SignUp') ?>
                                 <a class="list-group-item">
                                     First Name
                                     <input name="register-firstname" type="text" class="form-control" required>
