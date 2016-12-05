9e65e14f11c3556de720c3ea8bc1bf68d02b88b1
Pamintuan Elijah <nipahutresort16@gmail.com>
2016-12-05 18:45:59 +0000

    Update fix Registration

--- application/controllers/NipaHut_Controller.php
+++ application/controllers/NipaHut_Controller.php
@@ -9,7 +9,10 @@
 
 class NipaHut_Controller extends CI_Controller{
 
-
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
     public function index(){
         $this->load->database();
