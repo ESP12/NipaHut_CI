069532a3835c4307807decff067588c5809fb2bf
Pamintuan Elijah <nipahutresort16@gmail.com>
2016-12-08 20:53:23 +0000

    fix signup-success output

--- application/views/content/Registration/success-signUp.php
+++ application/views/content/Registration/success-signUp.php
@@ -7,36 +7,38 @@
 </section>
 
 <section class="reservation-content">
-    <div class="container"><div class="row"><div class="col-md-3"></div><div class="col-md-6">
-                <div class="panel panel-default"><div class="panel-heading summary-title">
-                        <h3 class="panel-title available-rooms-title">Guest Information - Sign Up</h3>
-                    </div>
+    <div class="container">
+        <div class="row">
+            <h1 class="text-center">Registration Successful <?php echo $_POST['firstname']; ?>!</h1><div class="row"><div class="col-md-3"></div><div class="col-md-6">
+                    <div class="panel panel-default"><div class="panel-heading summary-title">
+                            <h3 class="panel-title available-rooms-title">Guest Information</h3>
+                        </div>
                     <div class=" list-group">
                             <div class='alert alert-error span4'><?php echo validation_errors(); ?></div>
 
                         <a class="list-group-item">
                             <label>First Name</label>
-                            <input name="register-firstname" type="text" value="<?php echo $firstname;?>" class="form-control" required>
+                            <input name="register-firstname" type="text" value="<?php echo $_POST['firstname']; ?>" class="form-control" required>
                         </a>
                         <a class="list-group-item">
                             <label>Last Name</label>
-                            <input name="register-lastname" type="text" value="<?php  echo $lastname;?>" class="form-control" required>
+                            <input name="register-lastname" type="text" value="<?php echo $_POST['lastname']; ?>" class="form-control" required>
                         </a>
                         <a class="list-group-item">
                             <label>Gender</label>
-                            <input type="text" value="<?php echo $gender; ?>" class="form-control">
+                            <input type="text" value="<?php echo $_POST['gender']; ?>" class="form-control">
                         </a>
                         <a class="list-group-item">
                             <label>Email Address</label>
-                            <input name="register-emailaddress" value="<?php  echo $email;?>" type="text" class="form-control" required>
+                            <input name="register-emailaddress" value="<?php echo $_POST['email']; ?>" type="text" class="form-control" required>
                         </a>
                         <a class="list-group-item">
                             <label>Mobile Number</label>
-                            <input name="register-mobilenumber" value="<?php  echo $mobile?>" type="text" class="form-control" required>
+                            <input name="register-mobilenumber" value="<?php echo $_POST['mobile']; ?>" type="text" class="form-control" required>
                         </a>
                         <a class="list-group-item"><h6>*This part will be covered for your Nipa Hut Guest Account for your convenient access.</h6>
                             <label>Username</label>
-                            <input name="register-username" value="<?php  echo $username?>" type="text" class="form-control" required>
+                            <input name="register-username" value="<?php echo $_POST['username']; ?>" type="text" class="form-control" required>
                         </a>
                     </div>
                 </div>
