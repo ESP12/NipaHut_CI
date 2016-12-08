3dfcb3c06af2536c694a87a1c330233c26d111de
Pamintuan Elijah <nipahutresort16@gmail.com>
2016-12-08 21:28:28 +0000

    fix signup-success output

--- application/views/content/Registration/success-signUp.php
+++ application/views/content/Registration/success-signUp.php
@@ -8,7 +8,7 @@
 
 <section class="reservation-content">
     <div class="container">
-            <h1 class="text-center">Registration Successful <?php //echo $_POST['firstname']; ?>!</h1>
+            <h1 class="text-center">Registration Successful!</h1>
             <div class="row">
                 <div class="col-md-3"></div><div class="col-md-6">
                     <div class="panel panel-default">
