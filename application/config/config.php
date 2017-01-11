ae9ee97bc383cacd96647f29169ccb5e3ec85f2d
Pamintuan Elijah <nipahutresort16@gmail.com>
2017-01-11 13:44:56 +0000

    Repair Database

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://reservation-for-nipahut.herokuapp.com','';
+$config['base_url'] = 'https://reservation-for-nipahut.herokuapp.com';
 
 /*
 |--------------------------------------------------------------------------
